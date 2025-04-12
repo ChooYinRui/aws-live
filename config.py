b4a7035950a14e80060147dd301cb5a7c54b6687
ChooYinRui <chooyr-wm23@student.tarc.edu.my>
2025-04-13 02:08:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.co8g0fek1ueu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chooyinrui-employee"
+customregion = "us-east-1"
+
